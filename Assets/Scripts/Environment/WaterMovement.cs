d179b191f6014ad410f14f31afc93c871c80f8f0
Owen Davis-Bower <owendavisbower@gmail.com>
2026-04-19 18:01:01 -0700

    Lower water movement penalty area

--- Assets/Scripts/Environment/WaterMovement.cs
+++ Assets/Scripts/Environment/WaterMovement.cs
@@ -1,6 +1,6 @@
 public static class WaterMovement
 {
-    public const float SurfaceY = -0.245f;
+    public const float SurfaceY = -0.249f;
     public const float InWaterSpeedMultiplier = 0.5f;
 
     public static float SpeedMultiplier(float worldY) => worldY < SurfaceY ? InWaterSpeedMultiplier : 1f;
